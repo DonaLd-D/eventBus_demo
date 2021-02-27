ebb82c98c63b3cd2f09cf39ec4333d7d3c4f7a5c
DonaLd-D <57483340+DonaLd-D@users.noreply.github.com>
2021-02-27 23:33:12 +0800

    vue eventBus

---
+++ eventBus.js
@@ -0,0 +1,5 @@
+const Vue=require('vue')
+
+let eventBus=new Vue()
+
+module.exports={eventBus,Vue}
---
+++ index.js
@@ -0,0 +1,25 @@
+const {eventBus,Vue}=require('./eventBus.js') 
+global.$eventBus=new Vue
+
+let time=0,obj={id:time,name:"掘金"},arr=[]
+
+while(time<2){
+    eventBus.$emit('sendSomething',obj)
+
+    eventBus.$on('sendSomething',target=>{
+        arr.push(target)
+        console.log(arr)
+        time++
+    })
+}
+
+let _time=0, _obj={id:_time,name:'segmentfault'},_arr=[]
+while(_time<2){
+    $eventBus.$emit('sendSomestaff',_obj)
+
+    $eventBus.$on('sendSomestaff',target=>{
+        _arr.push(target)
+        console.log(_arr)
+        _time++
+    })
+}
